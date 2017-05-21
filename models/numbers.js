28821487c258334ee49c6f330510efadc4690744
Denis <carriere.denis@gmail.com>
2017-05-21 00:55:38 -0700

    Add numbers normalization

---
+++ models/numbers.js
@@ -0,0 +1,12 @@
+module.exports = new Map([
+    ['1st', 'first'],
+    ['2nd', 'second'],
+    ['3rd', 'third'],
+    ['4th', 'forth'],
+    ['5th', 'fifth'],
+    ['6th', 'sixth'],
+    ['7th', 'seventh'],
+    ['8th', 'eighth'],
+    ['9th', 'ninth'],
+    ['10th', 'tenth'],
+]);
