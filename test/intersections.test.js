1d0197834bf82328b6fcc1d891e710b64eaf5a04
Denis <carriere.denis@gmail.com>
2017-05-23 00:12:25 -0400

    Add JSdocs & tests to intersection

--- test/intersections.test.js
+++ test/intersections.test.js
@@ -1,30 +1,47 @@
 const {test} = require('tap');
 const {lineString} = require('@turf/helpers');
-const {createIndex} = require('../lib/create-index');
-const {intersections} = require('../lib/intersections');
+const {intersections, createUniques, filterUniques} = require('../lib/intersections');
 
 test('intersections', t => {
     const road1 = lineString([[10, 5], [10, 10]], {name: 'Foo Ave'});
     const road2 = lineString([[10, 5], [20, 30]], {name: 'Bar St'});
-    const intersects = intersections([road1, road2]);
-    const index = createIndex(intersects);
+    const uniques = intersections([road1, road2]);
+    //= Map { '10!5' => Set { { name: 'Foo Ave' }, { name: 'Bar St' } } }
 
-    t.equal(index.size, 8);
+    t.equal(uniques.size, 1);
+    t.equal(uniques.get('10!5').size, 2);
+    t.end();
+});
 
-    // True
-    t.true(index.has('foo avenue+bar street'));
-    t.true(index.has('foo+bar'));
-    t.true(index.has('foo+bar street'));
-    t.true(index.has('foo avenue+bar'));
-    t.true(index.has('bar street+foo avenue'));
-    t.true(index.has('bar+foo'));
-    t.true(index.has('bar+foo avenue'));
-    t.true(index.has('bar street+foo'));
+test('createUniques', t => {
+    const road1 = lineString([[10, 5], [10, 10]], {name: 'Foo Ave'});
+    const road2 = lineString([[10, 5], [20, 30]], {name: 'Bar St'});
+    const uniques = createUniques([road1, road2]);
+    // Map {
+    //    '10!5' => Set { { name: 'Foo Ave' }, { name: 'Bar St' } },
+    //    '10!10' => Set { { name: 'Foo Ave' } },
+    //    '20!30' => Set { { name: 'Bar St' } }
+    // }
 
-    // False
-    t.false(index.has('foo avenue+foo'));
-    t.false(index.has('foo+foo avenue'));
-    t.false(index.has('bar street+bar'));
-    t.false(index.has('bar+bar street'));
+    t.equal(uniques.size, 3);
+    t.equal(uniques.get('10!5').size, 2);
+    t.equal(uniques.get('10!10').size, 1);
+    t.equal(uniques.get('20!30').size, 1);
+    t.end();
+});
+
+test('filterUniques', t => {
+    const uniques = new Map([
+        ['10!5', new Set([{name: 'Foo Ave'}, {name: 'Bar St'}])],
+        ['10!10', new Set([{name: 'Foo Ave'}])],
+        ['20!30', new Set([{name: 'Bar St'}])],
+    ]);
+    const filtered = filterUniques(uniques);
+    //= Map { '10!5' => Set { { name: 'Foo Ave' }, { name: 'Bar St'} } }
+
+    t.equal(filtered.size, 1);
+    t.equal(filtered.get('10!5').size, 2);
+    t.false(filtered.has('10!10'));
+    t.false(filtered.has('20!30'));
     t.end();
 });
